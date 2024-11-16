c1b7fa5b61e9eba3b2c6408685644ab0d7c90c97
Umair <umairnehri9747@Gmail.com>
2024-11-16 18:44:06 +0530

    Update input.go

--- input.go
+++ input.go
@@ -4,21 +4,46 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
+	"strconv"
 	"strings"
+	"time"
 )
 
-var apiMode bool // New global flag for API mode
+var apiMode bool       // Global flag for API mode
+var reconMode bool     // Global flag for Recon mode
+var reconPorts []int   // Array of ports to scan in Recon mode
+var reconSubnet string // Variable for subnet in Recon mode
 
 func takeInput() {
-	// Define new `--api` flag
+	// Define `--api`, `--recon`, `--reconports`, and `--subnet` flags
 	flag.BoolVar(&apiMode, "api", false, "Run the tool in API mode")
+	flag.BoolVar(&reconMode, "recon", false, "Run the tool in Recon mode against local network")
 	flag.StringVar(&saveOutput, "save", "", "Save the output in a file. [Default filename is output.json]")
+	flag.StringVar(&reconSubnet, "subnet", "192.168.1.", "Specify the subnet for recon mode (e.g., 192.168.1., 10.0.0.)")
+	flag.Func("reconports", "List of ports to scan for each active host in recon mode", func(s string) error {
+		for _, p := range strings.Split(s, ",") {
+			port, err := strconv.Atoi(p)
+			if err != nil {
+				return err
+			}
+			reconPorts = append(reconPorts, port)
+		}
+		return nil
+	})
 	flag.Parse()
 	args = flag.Args()
 
 	if apiMode {
 		startAPIServer() // Start the API server if `--api` is passed
+		return
+	}
+
+	if reconMode {
+		log.Println("Recon mode activated. Scanning the local network...")
+		runReconMode() // Call recon mode function
 		return
 	}
 
@@ -63,4 +88,45 @@ func takeInput() {
 			}
 		}
 	}
+}
+
+func runReconMode() {
+	activeHosts := scanLocalNetwork(reconSubnet)
+	for _, host := range activeHosts {
+		if len(reconPorts) > 0 {
+			// Append ports if reconPorts flag is set
+			for _, port := range reconPorts {
+				target := fmt.Sprintf("http://%s:%d", host, port)
+				targets = append(targets, target)
+				log.Printf("Added host %s with port %d to targets", host, port)
+			}
+		} else {
+			// Just add the host if no ports specified
+			targets = append(targets, "http://"+host)
+			log.Printf("Added host %s to targets", host)
+		}
+	}
+	log.Println("Recon mode scan complete. Found targets:", targets)
+}
+
+func scanLocalNetwork(subnet string) []string {
+	var activeHosts []string
+	for i := 1; i <= 254; i++ {
+		ip := fmt.Sprintf("%s%d", subnet, i)
+		if pingHost(ip) {
+			log.Printf("Active host found: %s\n", ip)
+			activeHosts = append(activeHosts, ip)
+		}
+	}
+	return activeHosts
+}
+
+func pingHost(ip string) bool {
+	timeout := 1 * time.Second
+	cmd := exec.Command("ping", "-c", "1", "-W", fmt.Sprintf("%d", int(timeout.Seconds())), ip)
+
+	if err := cmd.Run(); err != nil {
+		return false // Host is not reachable
+	}
+	return true // Host is reachable
 }
